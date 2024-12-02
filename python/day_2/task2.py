eb3fa69c53b88c49d80b87fb8f1046306cb73c58
chris2pher1503 <christopher150304@outlook.com>
2024-12-02 18:57:47 +0100

    started on task 2

---
+++ python/day_2/task2.py
@@ -0,0 +1,32 @@
+count = 0
+safe = []
+
+with open("input.txt", "r") as file:
+    for line in file:
+        nums = list(map(int, line.split()))
+        sorted_nums_increase = sorted(nums)
+        sorted_nums_decrease = sorted(nums, reverse=True)
+        res_increase = sorted_nums_increase == nums
+        res_decrease = sorted_nums_decrease == nums
+
+        difference = []
+        for i in range(len(nums) - 1):
+            diff = abs(nums[i] - nums[i + 1])
+            if 1 <= diff <= 3:
+                difference.append(True)
+            else:
+                difference.append(False)
+
+        if (res_decrease or res_increase) and all(difference):
+            safe.append(nums)
+            count += 1
+
+        for i in range(len(nums)):
+            nums_removed = nums[:i] + nums[i + 1 :]
+            if safe(nums_removed):
+                safe.append(nums)
+                count += 1
+                break
+
+    print(safe)
+    print(count)
