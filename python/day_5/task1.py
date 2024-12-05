111b5f494a121ece029ff725f86ad4d874ca90f2
chris2pher1503 <christopher150304@outlook.com>
2024-12-05 17:45:34 +0100

    day 5 task 1

--- python/day_5/task1.py
+++ python/day_5/task1.py
@@ -1,12 +1,43 @@
-dicRule = {}
-
-with open("test.txt", "r") as file:
+rules_list = []
+count = 0
+with open("input.txt", "r") as file:
     content = file.read()
     matrix = content.split("\n\n")
     rules = matrix[0].split("\n")
     updates = matrix[1]
-    rules_part = rules.split("|")
-    print(rules_part)
+    for rule in rules:
+        rule = rule.split("|")
+        """ print("before: ", rule[0], " after: ", rule[1]) """
+        x = int(rule[0])
+        y = int(rule[1])
+        rules_list.append((x, y))
+
+    updates_list = updates.strip().split("\n")
+
+    for update in updates_list:
+        pages_str = update.strip().split(",")
+        pages = []
+        for x in pages_str:
+            pages.append(int(x))
+
+        side_pos = {}
+        index = 0
+        for page in pages:
+            side_pos[page] = index
+            index += 1
+
+        correct = True
+
+        for x, y in rules_list:
+            if x in side_pos and y in side_pos:
+                if side_pos[x] >= side_pos[y]:
+                    correct = False
+                    break
+        if correct:
+            middle = len(pages) // 2
+            if len(pages) % 2 == 0:
+                middle -= 1
+            count += pages[middle]
 
 
-print(rules)
+print(count)
